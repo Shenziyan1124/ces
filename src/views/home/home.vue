eedf67358b77957d400f8fd800a1849b3c3529c0
shenziyan <13383234395@163.com>
2020-05-30 10:14:48 +0800

    wo shi szy123

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,10 +1,12 @@
 <template>
   <el-container>
     <el-header>
-      <img src="@/views/login/image/LOGO-new.png" alt=""/>
-<!-- 
+      <img src="@/views/login/image/LOGO-new.png" alt />
+      <!-- 
       高速公路施工要素调查数据填报系统
-      【都香高速公路项目， 张三】 -->
+      【都香高速公路项目， 张三】-->
+
+      <div>woshi szy123</div>
     </el-header>
     <el-container>
       <el-aside width="600px">
@@ -14,36 +16,29 @@
           </div>
           <div class="left-card-body u_scrollBar">
             <el-table
-                :data="tableData1"
-                style="width: 100%"
-                row-key="id"
-                border
-                lazy
-                :highlight-current-row="true"
-                :load="load"
-                :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
-                tooltip-effect="dark"
-                @row-click="rowClick"
-                :cell-style="{padding:'2px 0'}"
-                :header-cell-style="{padding:'4px 0'}">
-              <el-table-column
-                  prop="name"
-                  label="名称"
-                  show-overflow-tooltip
-                  width="180">
-              </el-table-column>
-              <el-table-column
-                  prop="address"
-                  label="地址">
-              </el-table-column>
+              :data="tableData1"
+              style="width: 100%"
+              row-key="id"
+              border
+              lazy
+              :highlight-current-row="true"
+              :load="load"
+              :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+              tooltip-effect="dark"
+              @row-click="rowClick"
+              :cell-style="{padding:'2px 0'}"
+              :header-cell-style="{padding:'4px 0'}"
+            >
+              <el-table-column prop="name" label="名称" show-overflow-tooltip width="180"></el-table-column>
+              <el-table-column prop="address" label="地址"></el-table-column>
               <el-table-column label="操作" width="50" align="center">
                 <template slot-scope="scope">
                   <el-button
-                      type="text"
-                      size="mini"
-                      icon="iconfont icon-jia"
-                      @click="handleEdit(scope.$index, scope.row)">
-                  </el-button>
+                    type="text"
+                    size="mini"
+                    icon="iconfont icon-jia"
+                    @click="handleEdit(scope.$index, scope.row)"
+                  ></el-button>
                 </template>
               </el-table-column>
             </el-table>
@@ -91,118 +86,118 @@
 </template>
 
 <script>
-  let id = 1000;
-  export default {
-    name: "home",
-    data() {
-      return {
-        tableData1: [
-          {
-            id: 1,
-            name: '王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
-          }, {
-            id: 2,
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
-          }, {
-            id: 3,
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄',
-            hasChildren: true
-          }, {
-            id: 4,
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
-          }
-        ],
-        formInline: {
-          user: '',
-          region: ''
-        }
+let id = 1000;
+export default {
+  name: "home",
+  data() {
+    return {
+      tableData1: [
+        // {
+        //   id: 1,
+        //   name: '王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎王小虎',
+        //   address: '上海市普陀区金沙江路 1518 弄'
+        // }, {
+        //   id: 2,
+        //   name: '王小虎',
+        //   address: '上海市普陀区金沙江路 1517 弄'
+        // }, {
+        //   id: 3,
+        //   name: '王小虎',
+        //   address: '上海市普陀区金沙江路 1519 弄',
+        //   hasChildren: true
+        // }, {
+        //   id: 4,
+        //   name: '王小虎',
+        //   address: '上海市普陀区金沙江路 1516 弄'
+        // }
+      ],
+      formInline: {
+        user: "",
+        region: ""
       }
+    };
+  },
+  methods: {
+    load(tree, treeNode, resolve) {
+      setTimeout(() => {
+        resolve([
+          {
+            id: (id += 1),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+            hasChildren: true
+          },
+          {
+            id: (id += 2),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          },
+          {
+            id: (id += 3),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+            hasChildren: true
+          },
+          {
+            id: (id += 4),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          },
+          {
+            id: (id += 5),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+            hasChildren: true
+          },
+          {
+            id: (id += 6),
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄"
+          }
+        ]);
+      }, 1000);
     },
-    methods: {
-      load(tree, treeNode, resolve) {
-        setTimeout(() => {
-          resolve([
-            {
-              id: id += 1,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-              hasChildren: true
-            },
-            {
-              id: id += 2,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-            },
-            {
-              id: id += 3,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-              hasChildren: true
-            },
-            {
-              id: id += 4,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-            },
-            {
-              id: id += 5,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-              hasChildren: true
-            },
-            {
-              id: id += 6,
-              name: '王小虎',
-              address: '上海市普陀区金沙江路 1519 弄',
-            }
-          ])
-        }, 1000)
-      },
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      rowClick(row, column, event) {
-        console.log(row, column, event);
-      },
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    rowClick(row, column, event) {
+      console.log(row, column, event);
     }
   }
+};
 </script>
 
 <style scoped lang="scss">
-  .el-container {
-    .el-header {
-      background: #2a82e4;
-    }
-
-    .el-aside {
-      width: 600px;
-      padding: 5px;
-      height: calc(100vh - 60px);
-    }
-
-    .el-main {
-      padding: 5px;
-    }
+.el-container {
+  .el-header {
+    background: #2a82e4;
   }
+
+  .el-aside {
+    width: 600px;
+    padding: 5px;
+    height: calc(100vh - 60px);
+  }
+
+  .el-main {
+    padding: 5px;
+  }
+}
 </style>
 <style lang="scss">
-  .left-card {
-    height: 100%;
+.left-card {
+  height: 100%;
 
-    .el-card__body {
-      padding: 0;
-      height: calc(100% - 56px);
-      overflow: hidden;
-    }
-
-    .left-card-body {
-      padding: 0;
-      height: 100%;
-      overflow-y: scroll;
-    }
+  .el-card__body {
+    padding: 0;
+    height: calc(100% - 56px);
+    overflow: hidden;
   }
+
+  .left-card-body {
+    padding: 0;
+    height: 100%;
+    overflow-y: scroll;
+  }
+}
 </style>
